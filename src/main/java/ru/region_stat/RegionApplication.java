a2b47f1149cd5203eeb5ad721392cadae616d011
DmitryChebykin <78965652+DmitryChebykin@users.noreply.github.com>
2021-12-09 12:52:53 +0700

    init

---
+++ src/main/java/ru/region_stat/RegionApplication.java
@@ -0,0 +1,12 @@
+package ru.region_stat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegionApplication.class, args);
+    }
+}
